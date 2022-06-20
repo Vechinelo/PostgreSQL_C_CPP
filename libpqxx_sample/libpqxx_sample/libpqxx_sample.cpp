6f8cd95aabea8af43c04fecacbbe058853335c4c
Vechinelo <104919582+Vechinelo@users.noreply.github.com>
2022-06-20 15:48:13 +0300

    Add files via upload

---
+++ libpqxx_sample/libpqxx_sample/libpqxx_sample.cpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <string>
+#include <iostream>
+#include <pqxx\pqxx>
+using namespace std;
+
+int main()
+{
+   string conn = "host=localhost port=5432 dbname=tesdb user=postgres password =1234567890";
+
+    try
+    {
+        pqxx::connection connectionObject(conn.c_str());
+
+        pqxx::work worker(connectionObject);
+
+        pqxx::result res = worker.exec("SELECT * FROM public.customers");
+
+        for (size_t i = 0; i < res.size(); i++)
+        {
+            cout << "Id: " << res[i][0] << " Full name: " << res[i][1] << " Email: " << res[i][2]  << endl;
+        }
+        
+
+        worker.commit();
+
+    }
+    catch (const exception& e)
+    {
+        cerr << e.what() << endl;
+    }
+
+    system("pause");
+}

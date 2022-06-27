8812a03fb5468496ba5d3a2389a3183b82d3e986
Vechinelo <104919582+Vechinelo@users.noreply.github.com>
2022-06-27 22:20:58 +0300

    Add files via upload

---
+++ TestLIBPQ/TestLIBPQ/TestLIBPQ.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+//#include "iApcPostgreSQLDrv.h"
+#include <string>
+#include <chrono>
+#include<fstream>
+#include "tApcWorkObj.h"
+
+int main()
+{
+   
+    tApcWorkObj* spWorkObj = new tApcWorkObj();
+    if (!spWorkObj)
+    {
+        std::cout << "Can't create spWorkObj." << std::endl;
+        return -33;
+    }
+
+    spWorkObj->start();
+
+    return 0;
+}
+
---
+++ TestLIBPQ/TestLIBPQ/tApcWorkObj.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <fstream>
+
+const std::string logfilename = "logfile.txt";
+static std::ofstream fout;
+const std::string strConnect = "host=localhost port=5432 dbname=tesdb user=postgres password=1234567890";
+const int nTest = 30000;
+
+class tApcWorkObj
+{
+public:
+    tApcWorkObj();
+    ~tApcWorkObj();
+
+    int start();
+
+protected:
+
+    int write(int& anRes);
+    int read(int& anRes);
+    int writeBlob(int& anRes);
+    int readBlob(int& anRes);
+  /*  int writeTransaction(int& anRes);
+    
+    
+    int readBlob(int& anRes);*/
+};
+
